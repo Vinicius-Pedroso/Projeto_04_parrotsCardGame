7ac5aed8af6d3c3a88d62bfb55478f5ac4abf40d
Vinicius Pedroso <viniciuspedrosoreis@gmail.com>
2022-04-11 14:58:07 -0300

    Final commit, comentários

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+/*Variáveis globais que vou usar em várias funções */
 let cartasviradas = 0;
 let eliminatedcards = 0;
 let jogadas = 0;
 let time = 0;
+let todascartas = [];
+let card = {numero: 0, estado: 0};
 
+/*prompt inicial*/
 let numerocartas = Number(prompt("Com quantas cartas deseja jogar?"));
 while (numerocartas >14 || numerocartas <4 || numerocartas%2 !=0) {
     numerocartas = Number(prompt("Com quantas cartas deseja jogar? Por favor insira um número par entre 4 e 14!"))
 }
+/*Timer começando imediatamente depois do prompt*/
 timer();
-
-let todascartas = [];
-let card = {numero: 0, estado: 0};
 
 while (todascartas.length < numerocartas) {
     var novacarta = getRandomIntInclusive(1, numerocartas/2);
