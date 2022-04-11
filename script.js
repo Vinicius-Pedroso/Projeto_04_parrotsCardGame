89808673429e1658bfb3a1cafac406e24a300748
Vinicius Pedroso <viniciuspedrosoreis@gmail.com>
2022-04-11 14:33:31 -0300

    Fifth commit, Mobile + intervalo de 1 segundo pra virar de volta s 2 cartas + bonus

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let cartasviradas = 0;
 let eliminatedcards = 0;
 let jogadas = 0;
+let time = 0;
 
 let numerocartas = Number(prompt("Com quantas cartas deseja jogar?"));
 while (numerocartas >14 || numerocartas <4 || numerocartas%2 !=0) {
     numerocartas = Number(prompt("Com quantas cartas deseja jogar? Por favor insira um número par entre 4 e 14!"))
 }
+timer();
 
 let todascartas = [];
 let card = {numero: 0, estado: 0};
@@ -30,10 +32,13 @@ while (todascartas.length < numerocartas) {
 
 imprimircartas();
 
+
 function turncard(idcard) {
     todascartas[idcard].estado = 1;
     cartasviradas++;
     jogadas++;
+
+    imprimircartas();
 
     if (cartasviradas =2) {
         for (var j = 0; j < numerocartas; j++){
@@ -48,9 +53,23 @@ function turncard(idcard) {
                 }
             }
         }
-        imprimircartas();
+
+        setTimeout(function(){
+            imprimircartas();
+        }, 1000); 
+        
         if (eliminatedcards == numerocartas){
-            alert("você venceu");
+            setTimeout(function(){
+                alert("Você venceu em " +jogadas +" jogadas e levou " +time +" segundos para completar o jogo. Parabens!");
+                var string = prompt("Você deseja jogar novamente?");
+                if (string == "sim") {
+                    alert("Bom jogo")
+                    jogo ();
+                }
+                if (string == "não") {
+                    alert("Espero que tenha se divertido!")
+                }                
+            }, 1000);
         }
     }
 }
@@ -123,4 +142,50 @@ function imprimircartas() {
             }
         }
     }
+}
+
+function timer(){
+    setInterval(function(){
+        if (eliminatedcards < numerocartas) {
+            document.querySelector(".timerdiv").innerHTML = `
+            <h2>${time}s</h2>
+            `
+            time++;
+        } 
+    }, 1000);
+}
+
+function jogo(){
+    cartasviradas = 0;
+    eliminatedcards = 0;
+    jogadas = 0;
+    time = 0;
+    
+    numerocartas = Number(prompt("Com quantas cartas deseja jogar?"));
+    while (numerocartas >14 || numerocartas <4 || numerocartas%2 !=0) {
+        numerocartas = Number(prompt("Com quantas cartas deseja jogar? Por favor insira um número par entre 4 e 14!"))
+    }
+    timer();
+    
+    todascartas = [];
+    card = {numero: 0, estado: 0};
+    
+    while (todascartas.length < numerocartas) {
+        var novacarta = getRandomIntInclusive(1, numerocartas/2);
+        var contador = 0
+        var repetida = 0
+        while (contador < todascartas.length) {
+            if (novacarta == todascartas[contador].numero) {
+                repetida++;
+            }
+            contador++;
+        }
+        
+        card = {numero: novacarta, estado: 0};
+    
+        if (repetida < 2) {
+            todascartas.push(card);
+        }
+    }
+    imprimircartas();
 }
